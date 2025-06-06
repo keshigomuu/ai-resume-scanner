10e4e279c18547b13b615d8d3cc443df986d3f77
keshigomu <komanwai2004@gmail.com>
2025-06-06 13:55:02 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -31,7 +31,7 @@ document.getElementById("upload-form").addEventListener("submit", async (e) => {
       spinner.style.display = "block";
   
       // Call FastAPI endpoint
-      const response = await fetch("http://localhost:8000/match/", {
+      const response = await fetch("https://ai-resume-scanner-backend.onrender.com", {
         method: "POST",
         body: formData,
       });
